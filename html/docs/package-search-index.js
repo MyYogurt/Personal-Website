36570e5afd866b63a520171e626441f8ea479279
MyYogurt <panosmoisiadis@pm.me>
2022-03-10 14:50:32 -0500

    Update JavaDoc

--- html/docs/package-search-index.js
+++ html/docs/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"org.moisiadis"},{"l":"org.moisiadis.controller"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"org.moisiadis"},{"l":"org.moisiadis.controller"},{"l":"org.moisiadis.service"}];updateSearchResults();
